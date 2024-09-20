a993d692276165e7f24137914e8fcabf4dba678b
brn <brn.gdsn@gmail.com>
2024-09-20 14:40:19 -0400

    for gh-pages use hash router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TaskProvider from './context/TaskProvider';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
